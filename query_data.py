face08e7796e825c386a66ff82b4fe275b8fd9da
Michael LaCroix <michael.jp.lacroix@gmail.com>
2021-08-27 21:13:19 -0400

    working on getting sqlalchey to add to existing db

--- query_data.py
+++ query_data.py
@@ -2,6 +2,8 @@
 
 import psycopg2
 import config
+from sqlalchemy import create_engine
+import pandas as pd
 
 conn = psycopg2.connect(host="localhost", 
                         port = 5432, 
@@ -19,14 +21,24 @@ cur.execute("""
 query_results = cur.fetchall()
 print(query_results)
 
-cur.execute(r"""
-            COPY stocks(stock, date, closing_price)
-            FROM 'C:\Users\mlacroix_smartasset\data_project\tesla_prices.csv'
-            DELIMITER ','
-            CSV HEADER;
-            """)
-query_results = cur.fetchall()
-print(query_results)
+df = pd.read_csv(r'C:\Users\mlacroix_smartasset\data_project\tesla_prices.csv')
+print(df.head())
+print(df.shape)
+df.reset_index()
+
+engine = create_engine(f'postgresql://postgres:{config.postgresql_password}@localhost:5432/dvdrental')
+
+df.to_sql('stocks', engine, if_exists='append', index=False)
+
+# # data that wasn't executing due to permissions on the server side
+# cur.execute(r"""
+#             COPY stocks(stock, date, closing_price)
+#             FROM 'C:\Users\mlacroix_smartasset\data_project\tesla_prices.csv'
+#             DELIMITER ','
+#             CSV HEADER;
+#             """)
+# query_results = cur.fetchall()
+# print(query_results)
 
 
 cur.execute("""
